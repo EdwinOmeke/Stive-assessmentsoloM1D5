0ee83fda99b92aa48acae536e2b2dd4c0afb9c0a
EdwinOmeke <86473217+EdwinOmeke@users.noreply.github.com>
2021-06-28 10:32:18 +0100

    exB

--- Solo.js
+++ Solo.js
@@ -22,7 +22,9 @@ console.log(test)
 /* Ex.B
     Create a variable called "sum" and assign to it the result of the sum between 10 and 20.
 */
-
+let sumA = 10 + 20
+let sum = sumA
+console.log(sum)
 /* Ex.C 
     Create a variable called "random" and assign to it a random number between 0 and 20 (it should be randomly created at each execution).
 */
